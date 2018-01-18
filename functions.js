75ef9f73532d5a778c37a94db5b765632c3c1321
Sunny <sunny@serosensa.com>
2018-01-18 12:45:13 +0000

    amended results to show price in GBP

--- functions.js
+++ functions.js
@@ -1,11 +1,21 @@
-//var url = 'https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10';
-//var request = new XMLHttpRequest();
-//request.open('GET', url);
-//request.responseType = 'json';
-//request.send();
-//
-//
+// PUBLIC API METHODS 
 
+//Endpoint: /ticker/
+//Method: GET
+//Optional parameters:
+//(int) start - return results from rank [start] and above
+//(int) limit - return a maximum of [limit] results (default is 100, use 0 to return all results)
+//(string) convert - return price, 24h volume, and market cap in terms of another currency. Valid values are: 
+//"AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"
+
+//Example: https://api.coinmarketcap.com/v1/ticker/
+//Example: https://api.coinmarketcap.com/v1/ticker/?limit=10
+//Example: https://api.coinmarketcap.com/v1/ticker/?start=100&limit=10
+//Example: https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10
+
+//Limits
+//Please limit requests to no more than 10 per minute.
+//Endpoints update every 5 minutes.
 
  function createNode(element) {
       return document.createElement(element);
@@ -17,7 +27,7 @@
 
   const ul = document.getElementById('table');
 
-  const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=200';
+  const url = 'https://api.coinmarketcap.com/v1/ticker/?convert=GBP&limit=200';
   fetch(url).then(function(response) { 
 	// Convert to JSON
 	return response.json();
